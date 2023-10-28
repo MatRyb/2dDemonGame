aa071db79b995cfd6075ed73d417c862876abd0c
umbc1ok <umbcio@gmail.com>
2023-10-28 02:57:12 +0200

    Line collider more or less done

--- Assets/Scripts/TrailCollider.cs
+++ Assets/Scripts/TrailCollider.cs
@@ -9,6 +9,7 @@ public class TrailCollider : MonoBehaviour
     [SerializeField]
     private GameObject colliderDaddy; // father of all colliders
     List<GameObject> colliders;
+    List<GameObject> collidersQueue;
     [SerializeField]
     private GameObject player;
     [SerializeField]
@@ -21,6 +22,7 @@ public class TrailCollider : MonoBehaviour
     private void Start()
     {
         colliders = new List<GameObject>();
+        collidersQueue   = new List<GameObject>();
     }
     // Update is called once per frame
     void Update()
@@ -31,19 +33,18 @@ public class TrailCollider : MonoBehaviour
         {
             if(characterMove.GetVel()*i >= 7.0)
             {
-                if (newCollider != null)
-                {
-                    newCollider.SetActive(true);
-                    colliders.Add(newCollider);
-                    isWaitingToBePlaced = false;
-                }
                 newCollider = Instantiate(colliderDaddy);
                 newCollider.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
                 newCollider.transform.rotation = new Quaternion(player.transform.rotation.x, player.transform.rotation.y, player.transform.rotation.z, player.transform.rotation.w);
+                collidersQueue.Add(newCollider);
                 i = 0;
-                isWaitingToBePlaced = true;
-
             }
+        }
+        if(collidersQueue.Count > 2)
+        {
+            collidersQueue[i].SetActive(true);
+            colliders.Add(collidersQueue[0]);
+            collidersQueue.RemoveAt(0);
         }
     }
 }
